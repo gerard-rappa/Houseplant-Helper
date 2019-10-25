e9f8c4899c770e442d7bd4997ae6a4def79a4fcf
gerard-rappa <52970502+gerard-rappa@users.noreply.github.com>
2019-10-25 00:36:49 -0700

    Edited to process temperature and humidity as well

--- pi_duino.py
+++ pi_duino.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import serial
 
 port = "/dev/ttyACM0"
@@ -9,11 +8,21 @@ s1.flushInput()
 
 while True:
     if s1.inWaiting()>0:
-        inputValue=s1.readline().decode("utf-8")[:-2]
+        #Soil Moisture Sensor
+        inputValue=s1.readline().decode("utf-8")
         inputValue = int(inputValue)
-                
         if inputValue>550:
-            print("Dry", inputValue)
+            print("Very dry", inputValue)
         if inputValue<350:
-            print("wet", inputValue)
+            print("Very wet", inputValue)
+        #Temperature    
+        inputValue=s1.readline().decode("utf-8")
+        inputValue = float(inputValue)     
+        print("Temperature: ",inputValue," F")
+        
+        #Humidity
+        inputValue=s1.readline().decode("utf-8")
+        inputValue = float(inputValue)
+        print("Humidity: ",inputValue,"%")
+        
         
