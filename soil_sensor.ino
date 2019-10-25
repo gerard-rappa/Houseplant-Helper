fd467e212ca70eeb01befd33ce1462919b242385
gerard-rappa <52970502+gerard-rappa@users.noreply.github.com>
2019-10-25 00:35:42 -0700

    Added Temperature/Humidity Sensor

--- soil_sensor.ino
+++ soil_sensor.ino
@@ -1,9 +1,22 @@
+#include <dht.h>
+#define DHT11_PIN 3
+dht DHT;
+
 void setup(){
-   Serial.begin(9600); 
+
+  Serial.begin(9600);
 }
-void loop(){
-   int val;
-  val=analogRead(0);
-  Serial.println(val);
-  delay(100);
+void loop()
+{
+  int soil = analogRead(0);
+  int chk = DHT.read11(DHT11_PIN);
+  
+  //Soil Moisture Sensor
+  Serial.println(soil);
+  
+  //Temp/Humidity Sensor
+  //Celsius to Farenheit conversion
+  Serial.println((DHT.temperature * 1.8) + 32);
+  Serial.println(DHT.humidity);
+  delay(2000);
 }
